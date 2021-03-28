f2f7adda5362c492be310cc10154a0a4693efee2
utkarss_ingh <utkarshsingh.1306@gmail.com>
2021-03-28 13:10:02 +0530

    courses api updated

--- backend/controllers/teacherController.js
+++ backend/controllers/teacherController.js
@@ -1,4 +1,3 @@
-const generateToken = require('../utils/generateToken')
 const pool = require('../config/db')
 
 const getTeacherCohort = (req, res) => {
@@ -25,5 +24,29 @@ const getTeacherCohort = (req, res) => {
       })
 }
 
+const getTeacherCourses = (req, res) => {
+  const { cu_id } = req.body
+  pool.getConnection((err, conn) => {
+      if(err) res.status(400).send('Connection Error');
+      else {
+        let sql = `SELECT * FROM Course a INNER JOIN CurriculumDetails b ON a.CO_id=b.CO_id WHERE CU_id = ?;`
+        
+        conn.query(sql, [cu_id], (err, result) => {
+            if(err) res.status(400).send('Querry Error');
+            else {
+              if(result.length > 0) {
+                  res.json(result)
+              }
+              else {
+                  res.status(401)
+                  res.json({ message: "No Data Found" })
+              }
+            }
+            conn.release();
+          })
+        }
+    })
+}
 
-module.exports = { getTeacherCohort }
+
+module.exports = { getTeacherCohort, getTeacherCourses }
--- backend/routes/teacherRoutes.js
+++ backend/routes/teacherRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express')
-const { getTeacherCohort } = require('../controllers/teacherController.js')
+const { getTeacherCohort, getTeacherCourses } = require('../controllers/teacherController.js')
 const { protect } = require('../middleware/authMiddleware')
 
 const router = express.Router()
 
 router.route('/cohort').post(getTeacherCohort)
+router.route('/courses').post(getTeacherCourses)
+
 
 module.exports = router
