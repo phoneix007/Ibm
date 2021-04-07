2ffa97b2fde7f6c11bacc6c985be615d38337591
Vibhor Singh <vibhor.formal@gmail.com>
2021-04-07 11:57:21 +0530

    Sprint 2 complete

--- backend/controllers/studentController.js
+++ backend/controllers/studentController.js
@@ -4,33 +4,19 @@ const pool = require('../config/db')
 
 const getStudentCourses = (req, res) => {
   const { st_id } = req.body
-  console.log(`inside backend st id=${st_id}`)
-  pool.getConnection((err, conn) => {
+   pool.getConnection((err, conn) => {
       if(err) res.status(400).send('Connection Error');
       else {
-        /*let sql1 = `SELECT ch_id FROM COHORTSTUDENT WHERE _id = ?;`
-        conn.query(sql1, [st_id], (err, result) => {
-          if(err) res.status(400).send('Querry Error');
-          else {
-            if(result.length > 0) {
-                let ch_id=result;
-            }
-            else {
-                res.status(401)
-                res.json({ message: "No Data Found" })
-            }
-          }
-          conn.release();
-        })*/
-        let sql = `SELECT * FROM Course a INNER JOIN CurriculumDetails b ON a.CO_id=b.CO_id WHERE cu_id = ?;`
-        let ch_id=st_id;
-        console.log(`ch_id:${ch_id}`)
-        conn.query(sql, [ch_id], (err, result) => {
+       
+        
+        let sql = `SELECT * FROM Course a INNER JOIN CurriculumDetails b ON a.CO_id=b.CO_id WHERE cu_id = (SELECT CU_id FROM Cohort where CH_id = (SELECT CH_id FROM CohortStudent where ST_id=?));`
+        let cu_id=st_id;
+        conn.query(sql, [cu_id], (err, result) => {
             if(err) res.status(400).send('Querry Error');
             else {
               if(result.length > 0) {
                   res.json(result)
-                  console.log("123")
+                  //console.log(result)
               }
               else {
                   res.status(401)
