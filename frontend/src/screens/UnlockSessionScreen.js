535f79776394fffeb959be834a463defd7a78d5b
utkarss_ingh <utkarshsingh.1306@gmail.com>
2021-04-12 13:16:22 +0530

    sprint 3 final update

--- frontend/src/screens/UnlockSessionScreen.js
+++ frontend/src/screens/UnlockSessionScreen.js
@@ -31,7 +31,7 @@ export const UnlockSessionScreen = ({ history, match }) => {
         return false
     }
 
-    const funct = (ch_id, sp_id, co_id, tc_id, to_id) => {
+    const updateSessionStatus = (ch_id, sp_id, co_id, tc_id, to_id) => {
         window.location.reload()
         dispatch(unlockSession(ch_id, sp_id, co_id, tc_id, to_id))
     }
@@ -68,7 +68,7 @@ export const UnlockSessionScreen = ({ history, match }) => {
                             <td>{key.SP_Name}</td>
                             {checkStatus(key.SP_id) ? <Link to={`/sections`} onClick={() => dispatch(setTemp('sectionUrl', key.SP_id))}><td>View</td></Link> : 
                             <td>
-                                <Button size="sm" onClick={() => funct(urlParameter.cohortID, key.SP_id, key.CO_id, userInfo.TC_id, 1)}>Unlock</Button>
+                                <Button size="sm" onClick={() => updateSessionStatus(urlParameter.cohortID, key.SP_id, key.CO_id, userInfo.TC_id, 1)}>Unlock</Button>
                             </td>}
                         </tr>
                         )}
